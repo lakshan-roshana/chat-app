4ada5f1d595def3bb7f58571db9398f6159ba7c0
lakshan-roshana <lakshanroshana21@gmail.com>
2025-08-20 21:02:57 +0530

    signup. login, logout backend parts are  done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,93 @@
-export const signup =(req,res)=>{
-    res.send("Signup route");
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup =async (req,res)=>{
+    const {fullName,email,password} = req.body;
+    try{
+        if(!fullName || !email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+
+        if(password.length <6 ){
+            return res.status(400).json({message:"Password must be at least 6 characters long"});
+        }
+
+        const user = await User.findOne({email});
+
+        if(user) return res.status(400).json({message:"User already exists"});
+
+        const salt = await bcrypt.genSalt(10);
+
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const newUser = new User({
+            fullName,
+            email,
+            password:hashedPassword
+        });
+
+        if(newUser){
+            //generate jwt toekn
+            generateToken(newUser._id,res);
+            await newUser.save();
+            res.status(201).json({
+                _id:newUser._id,
+                fullName:newUser.fullName,
+                email:newUser.email,
+                profilePic:newUser.profilePic
+            });
+        }
+        else{
+            res.status(400).json({message:"User registration failed"});
+        }
+
+    }
+    catch(error){
+        console.log(`Signup Error: ${error.message}`);
+        res.status(500).json({message:"Internal server error"});
+    }
 };
 
-export const login =(req,res)=>{
-    res.send("Login route");
+export const login =async (req,res)=>{
+    const {email, password} = req.body;
+    try{
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({message:"User not found"});
+        }
+
+        const ispasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!ispasswordCorrect){
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+        generateToken(user._id,res);
+        res.status(200).json({
+            _id:user._id,
+            fullName:user.fullName,
+            email:user.email,
+            profilePic:user.profilePic
+        });
+    }
+    catch(error){
+        console.log(`Login Error: ${error.message}`);
+        res.status(500).json({message:"Internal server error"});
+    }
 };
 
 export const logout =(req,res)=>{
-    res.send("Logout route");
+    try{
+        res.cookie("jwt","",{
+            maxAge:0, // Set cookie to expire immediately
+            httpOnly:true,
+            sameSite:"strict",
+            secure: process.env.NODE_ENV !== "development" // Use secure cookies in production
+        });
+        res.status(200).json({message:"Logged out successfully"});
+    }
+    catch(error){
+        console.log(`Logout Error: ${error.message}`);
+        res.status(500).json({message:"Internal server error"});
+    }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,11 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app = express();
 
-app.use("/api/auth",authRoutes)
-
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/api/auth",authRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);

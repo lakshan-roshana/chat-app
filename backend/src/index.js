918ad7479e0001230ddf52214bcf103e74df850e
lakshan-roshana <lakshanroshana21@gmail.com>
2025-08-20 17:13:58 +0530

    mongodb database connected successfully

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
+dotenv.config();
 const app = express();
 
 app.use("/api/auth",authRoutes)
@@ -9,4 +12,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
-})
+    connectDB();
+});

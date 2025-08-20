c6e75136e463ac36f88c58fa1a20b577c14fe331
lakshan-roshana <lakshanroshana21@gmail.com>
2025-08-20 22:09:39 +0530

    backend auth part is done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -91,3 +91,33 @@ export const logout =(req,res)=>{
         res.status(500).json({message:"Internal server error"});
     }
 };
+
+export const updateProfile =async (req,res)=>{
+    try{
+        const {profilePic}  = req.body;
+        const userId = req.user._id;
+        
+        if(!profilePic){
+            return res.status(400).json({message:"Profile picture is required"});
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic:uploadResponse.secure_url},{new:true});
+
+        res.status(200).json(updatedUser);
+    }
+    catch(error){
+        console.log(`Update Profile Error: ${error.message}`);
+        res.status(500).json({message:"Internal server error"});
+    }
+};
+
+export const checkAuth = (req,res)=>{
+    try{
+        res.status(200).json(req.user);
+    }
+    catch(error){
+        console.log(`Check Auth Error: ${error.message}`);
+        res.status(500).json({message:"Internal server error"});
+    }
+}
